82e2a172c1b8748878cf199184717a3a5fe5be2e
adiseshan1505 <adiseshanramanan@gmail.com>
2026-01-05 23:30:48 +0530

    added signin logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -34,3 +34,34 @@ export async function signup(req, res) {
         });
     }
 }
+
+export async function signin(req,res){
+    const parsedData=signinSchema.safeParse(req.body);
+    if(!parsedData.success){
+        return res.status(403).json({
+            message: "Invalid input"
+        });
+    }
+    const {email, password}=parsedData.data;
+    const user=await UserModel.findOne({
+        email: email
+    });
+    if(!user){
+        return res.status(403).json({
+            message: "Invalid credentials"
+        });
+    }
+    const valid_pwd=await bcrypt.compare(password, user.password);
+    if(!valid_pwd){
+        return res.status(403).json({
+            message: "Invalid credentials"
+        });
+    } else{
+        const token=jwt.sign({
+            id: user._id
+        }, JWT_SECRET, {expiresIn: '7d'});
+        res.json({
+            message: token
+        });
+    }
+}
